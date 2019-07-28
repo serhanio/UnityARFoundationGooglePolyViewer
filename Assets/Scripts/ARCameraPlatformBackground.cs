c3377c653cd36c017d17e66e0e6dae0fe900a3d9
serhanio <sulkumen@usc.edu>
2019-07-27 18:46:10 -0700

    linear color support on Android camera background

---
+++ Assets/Scripts/ARCameraPlatformBackground.cs
@@ -0,0 +1,30 @@
+﻿//
+//
+// purpose: set camera background material depending on platform used
+//
+//
+// author: ge
+//
+// (c) 2019 innovation.rocks consulting gmbh
+//
+
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARCameraBackground))]
+public class ARCameraPlatformBackground : MonoBehaviour
+{
+    public Material _ARCoreBackground;
+    public Material _ARKitBackground;
+
+    void Start()
+    {
+        ARCameraBackground arcbg = GetComponent<ARCameraBackground>();
+        arcbg.useCustomMaterial = true;
+#if UNITY_IOS
+        arcbg.customMaterial = _ARKitBackground;
+#else
+        arcbg.customMaterial = _ARCoreBackground;
+#endif
+    }
+}
